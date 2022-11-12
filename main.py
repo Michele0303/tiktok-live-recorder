0600970abc1e23da1f136b01758984ed568d197b
Michele <michele8.fiori8@gmail.com>
2022-11-12 15:52:43 +0100

    Update main.py
    + New args management.
    + Auto Recorder Mode is available

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from time import gmtime, strftime
+import time
 import requests as req
 import re
+import argparse
+from argparse import RawTextHelpFormatter
 
+TIMEOUT = 5
 
 def banner() -> None:
     print("""
@@ -43,9 +47,17 @@ def get_live_url(room_id: str) -> str:
     return re.search('"liveUrl":"(.*?)"', content).group(1).replace("https", "http")
 
 
-def start_recording(live_url: str, output: str) -> None:
+def start_recording(user: str, room_id: str) -> None:
+    live_url_m3u8 = get_live_url(room_id)
+    live_url_flv = live_url_m3u8.replace("pull-hls", "pull-flv").replace("/playlist.m3u8", ".flv").replace("https", "http").replace(".m3u8", ".flv")
+    print("[*] URL M3U8", live_url_m3u8)
+    print("[*] URL FLV", live_url_flv)
+
+    current_date = strftime("%Y.%m.%d_%H-%M-%S", gmtime())
+    output = f"TK_{user}_{current_date}.flv"
+
     print("\n[*] RECORDING... ")
-    stream = req.get(live_url, stream=True, verify=False, timeout=15)
+    stream = req.get(live_url_flv, stream=True, verify=False, timeout=15)
     with open(output, "wb") as f:
         for chunk in stream.iter_content():
             f.write(chunk)
@@ -56,24 +68,47 @@ def start_recording(live_url: str, output: str) -> None:
 def main():
     banner()
 
-    print("USERNAME-> ", end='')
-    user = input()
+    user: str
+    mode: str
+
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-user",
+                        dest="user",
+                        help="record a live from the username.",
+                        action='store')
+    parser.add_argument("-mode",
+                        dest="mode",
+                        help="recording mode: (manual,automatic) [Default: manual]\n[manual] => manual live recording\n[automatic] => automatic live recording when the user is live).",
+                        default="manual",
+                        action='store')
+    args = parser.parse_args()
+
+    if not args.user:
+        raise Exception("[*] Missing user value")
+    if args.mode and args.mode != "manual" and args.mode != "automatic":
+        raise Exception("[*] Incorrect -mode value")
+    user = args.user
+    mode = args.mode
 
     room_id = get_room_id(user)
     print("[*] ROOM_ID:", room_id)
-    if not is_user_in_live(room_id):
-        print(f"\n[*] {user} is offline")
-        exit(0)
 
-    live_url_m3u8 = get_live_url(room_id)
-    live_url_flv = live_url_m3u8.replace("pull-hls", "pull-flv").replace("/playlist.m3u8", ".flv").replace("https", "http").replace(".m3u8", ".flv")
-    print("[*] URL M3U8", live_url_m3u8)
-    print("[*] URL FLV", live_url_flv)
+    if mode == "manual":
+        if not is_user_in_live(room_id):
+            print(f"\n[*] {user} is offline")
+            exit(0)
 
-    current_date = strftime("%Y.%m.%d_%H-%M-%S", gmtime())
-    output = f"TK_{user}_{current_date}.flv"
-    start_recording(live_url_flv, output)
+        start_recording(user, room_id)
 
+    if mode == "automatic":
+        while True:
+            if not is_user_in_live(room_id):
+                print(f"\n[*] {user} is offline")
+                print("waiting 5 minutes before recheck")
+                time.sleep(TIMEOUT * 60)
+                continue
+            
+            start_recording(user, room_id)
 
 if __name__ == '__main__':
     main()
