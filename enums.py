d0b8a1e4469b0d8504be724264dc6e333aa58dd2
Michele <michele8.fiori8@gmail.com>
2023-06-05 13:26:30 +0200

    V3.7
    - Fix/webcast request

--- enums.py
+++ enums.py
@@ -58,7 +58,7 @@ class Info(Enum):
     def __str__(self):
         return str(self.value)
 
-    VERSION = 3.6
+    VERSION = 3.7
     BANNER = f"""
 
   _____ _ _   _____    _     _    _           ___                   _         
