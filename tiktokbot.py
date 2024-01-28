070f3a6469ada901bb548a411c6cff863f4da205
Rafli Pasya <36727552+pasya1912@users.noreply.github.com>
2024-01-28 09:12:22 +0700

    Removing the accept header because its too long

--- tiktokbot.py
+++ tiktokbot.py
@@ -204,7 +204,6 @@ class TikTok:
         try:
             headers = {
                 'authority': 'www.tiktok.com',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'accept-language': 'en-US,en;q=0.9',
                 'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
                 'sec-ch-ua-mobile': '?0',
@@ -248,7 +247,6 @@ class TikTok:
         try:
             headers = {
                 'authority': 'www.tiktok.com',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'accept-language': 'en-US,en;q=0.9',
                 'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
                 'sec-ch-ua-mobile': '?0',
