7a6f47af21c34c5290aed93ed89b4fef6c2d490c
Rafli Pasya <36727552+pasya1912@users.noreply.github.com>
2024-01-28 09:07:50 +0700

    Fix Record by User & Link not working
    This was error due to the request dont have any header so i fix this by adding header to the request and it works.

--- tiktokbot.py
+++ tiktokbot.py
@@ -202,7 +202,22 @@ class TikTok:
         Given a url, get user and room_id.
         """
         try:
-            response = self.httpclient.get(self.url, allow_redirects=False)
+            headers = {
+                'authority': 'www.tiktok.com',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'en-US,en;q=0.9',
+                'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+            }
+
+            response = self.httpclient.get(self.url, allow_redirects=False, headers=headers)
             content = response.text
 
             if response.status_code == StatusCode.REDIRECT:
@@ -231,11 +246,29 @@ class TikTok:
         Given a username, I get the room_id
         """
         try:
-            response = self.httpclient.get(f"https://www.tiktok.com/@{self.user}/live", allow_redirects=False)
+            headers = {
+                'authority': 'www.tiktok.com',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'en-US,en;q=0.9',
+                'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+            }
+            response = self.httpclient.get(f"https://www.tiktok.com/@{self.user}/live", headers=headers,
+                                           allow_redirects=False)
+            #var_dump response
+
             if response.status_code == StatusCode.REDIRECT:
                 raise errors.Blacklisted('Redirect')
 
             content = response.text
+
             if "room_id" not in content:
                 raise ValueError()
 
