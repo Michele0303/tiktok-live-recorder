8ccecac286c79936a5bbdc480b84c4e7d9544854
michele0303 <michele8.fiori8@gmail.com>
2024-07-18 22:05:43 +0200

    Added headers to avoid waf (hope)

--- src/http_client.py
+++ src/http_client.py
@@ -15,7 +15,14 @@ class HttpClient:
     def configure_session(self) -> None:
         self.req = req.Session()
         self.req.headers.update({
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Sec-Ch-Ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\"",
+            "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Linux\"",
+            "Accept-Language": "en-US", "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.127 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1", "Sec-Fetch-Dest": "document",
+            "Priority": "u=0, i",
             "Referer": "https://www.tiktok.com/"
         })
 
