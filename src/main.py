3bc39b2ecca78fdf117479042c8675514effef48
Michele Fiori <31160531+Michele0303@users.noreply.github.com>
2024-11-15 23:29:25 +0100

    Cleanup

--- src/main.py
+++ src/main.py
@@ -210,10 +210,11 @@ def main():
 
 if __name__ == "__main__":
     # TODO: Convert to a class
-    from check_upts import check_updates #type: ignore
+    from check_updates import check_updates
 
     if check_updates():
         exit()
+
     # TODO: Add argument to disable update check
     # TODO: Add argument to force update check
     main()
