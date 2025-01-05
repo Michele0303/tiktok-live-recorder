e41ffe766bff7f2cf564ac99932f968c4884a677
wildmer <wilmerdiazolaya@gmail.com>
2025-01-05 01:05:55 -0500

    Add function to load Telegram configuration from telegram.json

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -16,6 +16,16 @@ def read_cookies():
     Loads the config file and returns it.
     """
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(script_dir, '..', 'cookies.json')
-    with open(config_path, 'r') as f:
+    config_path = os.path.join(script_dir, "..", "cookies.json")
+    with open(config_path, "r") as f:
+        return json.load(f)
+
+
+def read_telegram_config():
+    """
+    Loads the telegram config file and returns it.
+    """
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir, "..", "telegram.json")
+    with open(config_path, "r") as f:
         return json.load(f)
