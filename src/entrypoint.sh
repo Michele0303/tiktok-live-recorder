9bb2ef1553adbea852527e8f839ad421f2aff12e
Robin Meis <robin.meis@smartnoob.de>
2025-08-27 15:54:46 +0200

    Remove session cookie handling

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 set -e
 
-# Create cookies.json only if SESSIONID_SS is set
-if [[ -n "${SESSIONID_SS}" ]]; then
-  cat <<EOF > /app/src/cookies.json
-{
-  "sessionid_ss": "${SESSIONID_SS}",
-  "tt-target-idc": "useast2a"
-}
-EOF
-fi
-
 # Execute main.py with all passed arguments
 exec python3 main.py -no-update-check "$@"
