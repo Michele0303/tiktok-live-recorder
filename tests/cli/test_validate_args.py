f1e1ff1ae168ac18cf4a8d9e8d55b3074a6f2ad8
Michele Fiori <michele8.fiori8@gmail.com>
2026-07-06 23:38:18 +0200

    test: enhance argument validation tests for CLI modes

--- tests/cli/test_validate_args.py
+++ tests/cli/test_validate_args.py
@@ -5,25 +5,63 @@ import pytest
 
 from utils.args_handler import validate_and_parse_args
 from utils.custom_exceptions import ArgsParseError
+from utils.enums import Mode
 
 
-def test_validate_mode(monkeypatch):
+def test_manual_mode_valid_with_user(monkeypatch):
     monkeypatch.setattr(
         sys, "argv", ["tiktok-live-recorder", "-mode", "manual", "-user", "test"]
     )
-    validate_and_parse_args()  # Should not raise an exception
+    args, mode = validate_and_parse_args()
+    assert args.user == "test"
+    assert mode == Mode.MANUAL
 
+
+def test_automatic_mode_valid_with_user(monkeypatch):
     monkeypatch.setattr(
         sys, "argv", ["tiktok-live-recorder", "-mode", "automatic", "-user", "test"]
     )
-    validate_and_parse_args()  # Should not raise an exception
+    args, mode = validate_and_parse_args()
+    assert args.user == "test"
+    assert mode == Mode.AUTOMATIC
 
+
+def test_followers_mode_valid_with_user(monkeypatch):
+    # User input is not required for followers mode
     monkeypatch.setattr(
         sys, "argv", ["tiktok-live-recorder", "-mode", "followers", "-user", "test"]
     )
-    validate_and_parse_args()  # Should not raise an exception
+    args, mode = validate_and_parse_args()
+    assert args.user == "test"
+    assert mode == Mode.FOLLOWERS
 
-    # Test for invalid mode
+
+def test_manual_mode_valid_without_user(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["tiktok-live-recorder", "-mode", "manual"])
+    with pytest.raises(
+        ArgsParseError,
+        match="Missing URL, username, or room ID. Please provide one of these parameters.",
+    ):
+        validate_and_parse_args()  # Should not raise an exception
+
+
+def test_automatic_mode_valid_without_user(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["tiktok-live-recorder", "-mode", "automatic"])
+    with pytest.raises(
+        ArgsParseError,
+        match="Missing URL, username, or room ID. Please provide one of these parameters.",
+    ):
+        validate_and_parse_args()
+
+
+def test_followers_mode_valid_without_user(monkeypatch):
+    # User input is not required for followers mode
+    monkeypatch.setattr(sys, "argv", ["tiktok-live-recorder", "-mode", "followers"])
+    _, mode = validate_and_parse_args()  # Should not raise an exception
+    assert mode == Mode.FOLLOWERS
+
+
+def test_invalid_mode(monkeypatch):
     monkeypatch.setattr(
         sys, "argv", ["tiktok-live-recorder", "-mode", "x", "-user", "test"]
     )
@@ -31,4 +69,4 @@ def test_validate_mode(monkeypatch):
         ArgsParseError,
         match="Incorrect mode value. Choose between 'manual', 'automatic' or 'followers'.",
     ):
-        validate_and_parse_args()  # Should raise an exception for unknown mode
+        validate_and_parse_args()  # Should raise an ArgsParseError for unknown mode
